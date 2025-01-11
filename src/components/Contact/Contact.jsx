22b12e766204855808bd05e616f9a41132264861
Denys Severin <severindenys367@gmail.com>
2025-01-11 11:21:26 +0100

    small footer adjustments to icons and text

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,26 +13,25 @@ const Contact = () => {
       </div>
       <ul className="contact__links">
         <li className="contact__link">
-          <img src={emailIcon} alt="email icon" />
           <a href="mailto:denys.severin@gmx.ch" target="_blank">
-            Email
+            <img src={emailIcon} alt="email icon" />
           </a>
+          <p>Email</p>
         </li>
         <li className="contact__link">
-          <img src={whatsAppIcon} alt="whatsapp icon" />
           <a
             href="https://wa.me/410796020670?text=Hello%20there!"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            WhatsApp
+            <img src={whatsAppIcon} alt="whatsapp icon" />
           </a>
+          <p>WhatsApp</p>
         </li>
         <li className="contact__link">
-          <img src={githubIcon} alt="github icon" />
           <a href="https://github.com/SeverinDenys" target="_blank">
-            Github
+            <img src={githubIcon} alt="github icon" />
           </a>
+          <p>Github</p>
         </li>
       </ul>
     </footer>
